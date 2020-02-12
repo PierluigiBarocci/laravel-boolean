41f5f6454bdeeaa1d5a48ea7454a8abab77b39e2
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2020-02-12 12:47:28 +0100

    Migliorata la gestione della classe Active nella navbar

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -7,16 +7,16 @@
                 </a>
                 <div class="boolean__navbar__items collapse navbar-collapse">
                     <ul class="navbar-nav">
-                        <li class="boolean__navbar__item nav-item active">
+                        <li class="boolean__navbar__item nav-item {{ Route::currentRouteName() === 'homepage' ? 'active' : '' }}">
                             <a class="nav-link" href="{{ route('homepage') }}">Home</a>
                         </li>
-                        <li class="boolean__navbar__item nav-item">
+                        <li class="boolean__navbar__item nav-item {{ Route::currentRouteName() === 'corso' ? 'active' : '' }}">
                             <a class="nav-link" href="#">Corso</a>
                         </li>
-                        <li class="boolean__navbar__item nav-item">
+                        <li class="boolean__navbar__item nav-item {{ Route::currentRouteName() === 'after' ? 'active' : '' }}">
                             <a class="nav-link" href="#">Dopo il corso</a>
                         </li>
-                        <li class="boolean__navbar__item nav-item">
+                        <li class="boolean__navbar__item nav-item {{ Route::currentRouteName() === 'lezione' ? 'active' : '' }}">
                             <a class="nav-link" href="#">Lezione Gratuita</a>
                         </li>
                         <li class="boolean__navbar__cta nav-item">
