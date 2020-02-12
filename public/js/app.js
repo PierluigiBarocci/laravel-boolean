9fd21a7fcbd77182e3b3b31a459c92fad8118018
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2020-02-12 15:46:36 +0100

    Domande Frequenti completata, slideDown e slideUp con jQuery

--- public/js/app.js
+++ public/js/app.js
@@ -29895,7 +29895,26 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
 var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
-$(document).ready(function () {// alert('ciao');
+$(document).ready(function () {
+  $('.boolean__faq__body__question h3 i').click(function () {
+    $('.boolean__faq__body__question p').slideUp();
+
+    if ($(this).hasClass('fa-plus')) {
+      $('.boolean__faq__body__question i').removeClass('fa-minus');
+      $('.boolean__faq__body__question i').addClass('fa-plus');
+      $(this).removeClass('fa-plus');
+      $(this).addClass('fa-minus');
+      var father = $(this).closest('div');
+      father.find('p').slideDown();
+    } else {
+      $(this).removeClass('fa-minus');
+      $(this).addClass('fa-plus');
+      var father = $(this).closest('div');
+      father.find('p').slideUp();
+    }
+
+    ;
+  });
 });
 
 /***/ }),
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,20 @@
 require('./bootstrap');
 var $ = require('jquery');
 $(document).ready(function(){
-    // alert('ciao');
+    $('.boolean__faq__body__question h3 i').click(function(){
+        $('.boolean__faq__body__question p').slideUp();
+        if ($(this).hasClass('fa-plus')) {
+            $('.boolean__faq__body__question i').removeClass('fa-minus');
+            $('.boolean__faq__body__question i').addClass('fa-plus');
+            $(this).removeClass('fa-plus');
+            $(this).addClass('fa-minus');
+            var father = $(this).closest('div');
+            father.find('p').slideDown();
+        } else {
+            $(this).removeClass('fa-minus');
+            $(this).addClass('fa-plus');
+            var father = $(this).closest('div');
+            father.find('p').slideUp();
+        };
+    })
 })
